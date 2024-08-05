ba80f83542e5b96806332a60fcb3766d20d60619
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-08-06 01:41:42 +0300

    footer-active-link

---
+++ src/js/footer.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const currentPath = window.location.pathname;
+
+  const privacyLink = document.querySelector(
+    '.footerNavLink[href="./page-2.html"]'
+  );
+  const termsLink = document.querySelector(
+    '.footerNavLink[href="./page-3.html"]'
+  );
+
+  console.log('Privacy Link: ', privacyLink);
+  console.log('Terms Link: ', termsLink);
+  console.log('Current Path: ', currentPath);
+
+  // Используем метод endsWith для проверки, что текущий путь заканчивается на '/page-2.html' или '/page-3.html'
+  if (currentPath.endsWith('/page-2.html') && privacyLink) {
+    privacyLink.classList.add('disabled');
+  } else if (currentPath.endsWith('/page-3.html') && termsLink) {
+    termsLink.classList.add('disabled');
+  }
+});
